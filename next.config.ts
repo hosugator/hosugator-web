1418fda583d09dfd78f0cf2916e478fe160125b7
Hong Seung Wan <hong@hongui-MacBookAir.local>
2026-01-15 22:30:40 +0900

    feat: AWS connection

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+// next.config.ts
+const nextConfig = {
+  output: 'export', // 서버 없이 실행 가능한 정적 파일들만 추출하도록 설정합니다.
+  images: {
+    unoptimized: true, // Next.js의 이미지 최적화 서버를 사용할 수 없는 환경(S3)을 위한 설정입니다.
+  },
 };
 
 export default nextConfig;
